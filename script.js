b95089da0ae462c7e26ff2f7af5d4c40634c57f2
2024103030042 <ariane.o@estudante.ifro.edu.br>
2025-09-05 11:38:16 -0400

    05/09

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const lcond = document.querySelector('#lCond');
 const input =  document.querySelector('input');
 const p =  document.querySelector('p');
 const produtoAdd= document.querySelector('#produtoAdd');
+const lista = document.querySelectorAll('li');
+const listaEd = document.querySelectorAll('#listaEs');
+
 
 
 lcond.addEventListener('mouseover', () => {
@@ -26,7 +29,16 @@ produtoAdd.addEventListener('mousemove', () =>{
     let b = parseInt (Math.random()*255);
     let cor = `rgb(${r}, ${g}, ${b})`
     console.log(cor);
+    produtoAdd.style.backgroundColor = cor;
 
 });
+lista.forEach((elemento) => {
+    //console.log(elemento);
+    elemento.addEventListener('click', (evento) =>{
+        console.log(evento.target.innerText)
+        console.log(evento.target.parentElement);
+        if evento in  
 
+    })
 
+})
